c96df09e222d599ebdea1f29e453227d4a1bf77e
bashar-alasmar <basharalasmar96@gmail.com>
2021-03-28 13:23:17 +0300

    Changes

--- javascript/app.js
+++ javascript/app.js
@@ -21,9 +21,9 @@ DataTypes :
 
 
 */
-var userName = prompt('what is your name? ');
+// var userName = prompt('what is your name? ');
 
-alert('Welcome ' + userName);
+// alert('Welcome ' + userName);
 // 'Hello my name is bashar'
 // "This is a String data type"
 // '  3+3 '
@@ -55,33 +55,33 @@ alert('Welcome ' + userName);
 // 1. To show the errors if we have ! 
 // its for the developers
 
-var firstName = 'Bashar ';
-var lastName = 'Alasmar';
-var x = 10;
-var i = 20; 
+// var firstName = 'Bashar ';
+// var lastName = 'Alasmar';
+// var x = 10;
+// var i = 20; 
 
-// console.log(x*i);
-// console.log(firstName);
+// // console.log(x*i);
+// // console.log(firstName);
 
-//             Concatincation ! 
+// //             Concatincation ! 
 
-var fullName = firstName + lastName;
-// console.log(fullName);
+// var fullName = firstName + lastName;
+// // console.log(fullName);
 
 
 
-// Comparison operators
-// == compare two values 
-// === Strictly equal check everything the data type and the value
-// Greater Than > 
-// less Than < 
-//Greater Than or Equal >=
-//Less thhan or Equal  <= 
-// Not equl !=
+// // Comparison operators
+// // == compare two values 
+// // === Strictly equal check everything the data type and the value
+// // Greater Than > 
+// // less Than < 
+// //Greater Than or Equal >=
+// //Less thhan or Equal  <= 
+// // Not equl !=
 
-var x = 20; 
+// var x = 20; 
 
-var y = '20';
+// var y = '20';
 
 // console.log(x === y); // is x is equal y ? 
 
@@ -123,25 +123,226 @@ if(condition){
 
 
 
-/*
 
-1- Ask the user what is your favourite instrument (3 images)!
 
-2- show image based on the Answer 
 
+
+
+//*************************************************************CLASS05 */
+// Logical operators:
+// -AND &&: the condition is only true when BOTH of them are true. (shift + 7)
+// -OR || : the condition is true when ONE of them is true. (shift + backslash)
+// loops:
+// a way to repeat code for an amount of time 
+
+// Loops: programming stucture, repeat a block of code until a specific condition is met!
+
+//Why? reduce redunedent code 
+// without copying and pasting the code over and over again
+// While: 
+// when you want to repeat the code for an UNLIMTED amount of time until the condition
+// is false
+// while(condition){
+//     while the condition is true keep running this code, if its false break the loop
+// }
+
+
+
+
+
+
+
+// i want to take the user age
+// if his age is greater or equal to 18 and his age is less than 35 i will welcome to my website 
+// if his age is less than 18 or his is greater than 35 i will tell him you're not allowed 
+// default case i will just tell him to have a nice day ! 
+
+
+// var age = prompt('whats your age?');
+// // 12
+//         // F       // T 
+// if(age >= 18 && age < 35){ // F
+//     alert('Welcome to my website!')
+//     // T  || F 
+// }else if(age < 18 || age > 35){ // T 
+//     alert('Sorry your not allowed!')
+// }else{
+//     alert('Have a nice day!')
+// }
+
+
+
+/* 
+
+while(condition){
+
+    code .. 
+
+}
 
 */
 
 
-var favInstrument = prompt('Whats your favourite instrument!','piano , guitar , violin');
 
-if(favInstrument === 'guitar'){
-    document.write('<img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/DCTM_Penguin_UK_DK_AL1007936_rt_q2qwiw.jpg" />')
-}else if(favInstrument === 'piano'){
-    document.write('<img src="https://www.musikalessons.com/blog/wp-content/uploads/2017/06/grand-piano-.jpg"/>')
-}else if (favInstrument === 'violin'){
-    document.write('<img src="https://cdn.shopify.com/s/files/1/0182/0563/products/MaestroVNTop_682x1800_7159c927-5752-43e0-9c2e-a19df87bf18b_800x.JPG?v=1551819005"/>')
-}else {
-    alert('Okay welcome to my website')
+// if the password is not uncommon i wont allow the user to enter my website ! 
+
+// var userPassword = prompt('Guess the password!');
+// var password = 'uncommon';
+
+// // anything !
+//         // anything !== uncommon 
+//  // out of the if statement !
+//             // T   // until the user enter the correct password(uncommon)
+//             //anything !== 'uncommon' T 
+// while(userPassword !== password){ // validation 
+//     userPassword = prompt('Please enter the correct password!')
+//     // we're not assigning the value to anywhere
+// }
+// while(userPassword !== password){ // validation 
+//     prompt('Please enter the correct password!')
+//     // we're not assigning the value to anywhere
+// }
+
+// while validating the user input ! 
+// when i dont know how many times i am going to loop
+
+
+
+
+/*
+
+1- Ask the user what is your favourite instrument!
+
+2- keep asking until he enters piano or guitar 
+
+3- check how many instruments he wants
+ 
+4- show a number of images based on the user enter 
+
+
+// */
+// var favInstrument = prompt('Whats your favourite instrument!','piano , guitar');
+
+// // guitar
+//         // T  || F 
+//         // T 
+
+// // guitar 
+//                 // T  &&  F 
+//                 //  F 
+                
+// // Bashar          T                 T       
+// while(favInstrument !== 'piano' && favInstrument !== 'guitar'){
+
+//     favInstrument = prompt('please enter on of the following guitar or piano');
+//     // guitar no where ! 
+// }
+// // guitar 
+//                 // F            ||    //      T 
+
+// guitar   // F            &&     // T 
+
+// anything  F             ||      F
+ // !F  = > T 
+//  while(!(favInstrument ==='piano' || favInstrument ==='guitar')){ 
+//     favInstrument = prompt('please enter on of the following guitar or piano');
+// }
+
+
+// i want to print in the console.log 10 numbers 
+                // 0 <= 10
+                // // T 
+                // 10
+// for(var i = 1 ; i<=10 ; i++ ){
+
+//     console.log(i); // 0  1 
+
+// }
+
+                //100 < 100 => False
+// for(var i = 0 ; i < 100; i = i + 10){
+//     console.log('Your website is loadig ', i)
+// }
+
+// console.log(i);
+
+// 0
+// show nothing 
+// 0 - 9
+// nothing 
+                // F 
+// for(var x = 0 ; x === 10 ; x++){
+//     console.log(x);
+// }
+
+// console.log(3 + '4') as a string
+// console.log('the last value of i : ' +  i)
+// console.log(i);
+// console.log('the last value of i : ' ,  i)
+
+
+// for(var j = 50 ; j >= 30 ; j--){
+//     console.log(j);
+// }
+
+// for(var x = 0 ; x <=10 ; x++){
+//     console.log(x)
+// }
+
+// var i =0
+// while(i <= 10){
+
+//     console.log(i)
+//     i++;
+// }
+
+
+// for(var i = 0 ; i < 5; i++){
+//     console.log('outer loop ' , i );
+//     for(var j = 0 ; j < 10 ; j++){
+//             console.log('inner loop', j)
+//     }
+// }
+
+
+
+
+// 1- Ask the user what is your favourite instrument!
+
+// 2- keep asking until he enters piano or guitar 
+
+// 3- check how many instruments he wants
+ 
+// 4- show a number of images based on the user enter 
+
+
+
+var favInstrument = prompt('Whats your favourite instrument!','piano , guitar');
+     
+while(favInstrument !== 'piano' && favInstrument !== 'guitar'){
+
+    favInstrument = prompt('please enter on of the following guitar or piano');
 }
 
+// guitar 
+// how many guitars would you like to see? 
+// 20 images of the guitar!
+
+
+var instrumentsNum = prompt('How many guitar or piano would you like to see?');
+// 4 
+
+var image = ''; 
+// undifined
+
+// declaring a variable 
+                       // 
+for(var i = 1 ; i <=instrumentsNum; i++ ){
+    if(favInstrument === 'guitar'){
+        image = image + '<img src="images/guitar.png"/>';
+    }else if(favInstrument === 'piano'){
+        image = image + '<img src="images/piano.jpg"/>';
+    }
+}
+console.log(image);
+document.write(image); // to show the image!
